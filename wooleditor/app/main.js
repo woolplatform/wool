fd465cbc32f5acad2ec1b1ae8787cc3f278bb102
Boris van Schooten <B.vanSchooten@rrd.nl>
2020-03-17 13:20:51 +0100

    Changed path to index.html to be Mac-compatible.

--- wooleditor/app/main.js
+++ wooleditor/app/main.js
@@ -15,7 +15,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile(app.getAppPath()+'/wooleditor/app/index.html')
 }
 
 //window.$ = window.jQuery = require('wooleditor/js/libs/jquery-1.11.2.min.js');
