a86ea7179be0fed71ae9f349fa29671f00d3336d
Harm op den Akker <hopdenakker@innovationsprint.eu>
2022-05-16 16:16:35 +0200

    Initial commit of SpringInitializr project

---
+++ java/wool-variable-service/src/main/java/eu/woolplatform/woolvariableservice/WoolVariableServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.woolplatform.woolvariableservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoolVariableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoolVariableServiceApplication.class, args);
+	}
+
+}
---
+++ java/wool-variable-service/src/test/java/eu/woolplatform/woolvariableservice/WoolVariableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.woolplatform.woolvariableservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoolVariableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
