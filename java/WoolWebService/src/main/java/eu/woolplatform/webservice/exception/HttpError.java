b52b185740881200697171fca1108a84ce26db1c
Harm op den Akker <hopdenakker@innovationsprint.eu>
2022-06-14 14:36:50 +0200

    WOOL Web Service: Updated License comments

--- java/WoolWebService/src/main/java/eu/woolplatform/webservice/exception/HttpError.java
+++ java/WoolWebService/src/main/java/eu/woolplatform/webservice/exception/HttpError.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2019-2022 WOOL Foundation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 package eu.woolplatform.webservice.exception;
 
 import java.util.ArrayList;
@@ -6,7 +27,7 @@ import java.util.List;
 import eu.woolplatform.utils.json.JsonObject;
 
 /**
- * A HTTP error can be included as a JSON string in the content of a HTTP error
+ * A HTTP error can be included as a JSON string in the content of an HTTP error
  * response. It has an optional error code. The core error codes are defined as
  * constants in {@link ErrorCode ErrorCode}.
  * 
